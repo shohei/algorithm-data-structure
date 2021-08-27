71ac0b0af3fe5cf99d0574a53ff06e78d3df75e3
Shohei Aoki <shoaok@gmail.com>
2021-08-27 16:43:08 +0300

    add depth-first search and breadth-first search

---
+++ graph/search.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+def search(G,s):
+    N = len(G)
+    seen = [False]*N
+    todo = []
+
+    seen[s] = True
+    todo.append(s)
+
+    while(todo!=[]):
+        print(todo)
+        #stack -> depth-first search
+        v = todo[-1]
+        todo = todo[:-1] 
+        #queue -> breadth-first search
+        #v = todo[0]
+        #todo = todo[1:] 
+    
+        for x in G[v]:
+            if (seen[x]):
+                continue
+            seen[x] = True
+            print(x,"True")
+            todo.append(x)
+    return seen
+
+G = [(1,4,2),(0,3,8),(0,5),(1,7,8),(0,8),(2,6,8),(5,7),(3,6),(1,3,4,5)]
+print(search(G,0))
+
+
